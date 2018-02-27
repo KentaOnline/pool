94ed59150181aa8e49e917bba42c4778c7c35f4a
linescoin <metaln33@gmail.com>
2018-02-27 11:37:48 +0000

    initial commit

---
+++ website/config.js
@@ -0,0 +1,25 @@
+var api = "http://pool.lines.click:8117";
+
+var api_blockexplorer = "http://explorer.lines.click:15011";
+
+var poolHost = "pool.lines.click";
+
+var irc = "";
+
+var email = "support@pool.lines.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.lines.click/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.lines.click/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "lns": [
+        ["pool.lines.click", "http://pool.lines.click:8117"],
+    ]
+};
