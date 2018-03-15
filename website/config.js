f580a2154eff6a6e8e59ca8cfe0b8f4da85fb386
linescoin <metaln33@gmail.com>
2018-03-15 14:23:07 +0000

    website fixes, domain change

--- website/config.js
+++ website/config.js
@@ -1,25 +1,25 @@
-var api = "http://pool.lines.click:8117";
+var api = "http://pool.lines.pw:8117";
 
-var api_blockexplorer = "http://explorer.lines.click:15011";
+var api_blockexplorer = "http://explorer.lines.pw:15015";
 
-var poolHost = "pool.lines.click";
+var poolHost = "pool.lines.pw";
 
 var irc = "";
 
-var email = "support@pool.lines.com";
+var email = "pool@lines.pw";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://explorer.lines.click/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://explorer.lines.pw/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://explorer.lines.click/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://explorer.lines.pw/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = {
     "lns": [
-        ["pool.lines.click", "http://pool.lines.click:8117"],
+        ["pool.lines.pw", "http://pool.lines.pw:8117"],
     ]
 };
